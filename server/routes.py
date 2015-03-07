c07788cf1fb1551204cdc519d0f422d017640c01
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-07 02:23:28 -0700

    Working on Objects and Database posts

--- server/routes.py
+++ server/routes.py
@@ -1,34 +1,10 @@
 from __init__ import app, db
 from subprocess import call
 from models import User
-"""
-"""
 
 from flask import request
 from flask import abort
 from flask import jsonify
-
-objects = []
-
-@app.route('/create', methods=['POST'])
-def create_object():
-    if not request.json or not 'important' in request.json:
-        abort(400)
-    if len(objects) == 0:
-        someobject = {
-        'id': 0,
-        'important': request.json['important'],
-        'nonreq': request.json.get('nonreq', "no nonreq input"),
-        }
-    else:
-        someobject = {
-        'id': objects[-1]["id"] + 1,
-        'important': request.json['important'],
-        'nonreq': request.json.get('nonreq', "no nonreq input"),
-        }
-
-    objects.append(someobject)
-    return jsonify({'object': someobject}), 201
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -36,27 +12,26 @@ def register():
         abort(400) # Malformed Packet
     guid = request.json['guid']
     user = User(guid)
+    db.session.add(user)
     db.session.commit()
 
     registerObject = {
     'id': user.guid
     }
 
-    # Do something with registerObject here
-
     return jsonify(registerObject), 201
-
-import hashlib
 
 @app.route('/phone', methods=['POST'])
 def phone():
     if not request.json or (not ('call-time' in request.json)) or (not ('id' in request.json)):
         abort(400) # Malformed Packet
 
-    if not True: #Check database for id to make sure it exists
+    user = User.query.filter_by(id=request.json["id"]).first()
+
+    if not user: #Check database for id to make sure it exists
         abort(401)
 
-    # TODO Add calltime to the user that matches the id
+    # Todo Steve will do this
 
     return "", 201
 
