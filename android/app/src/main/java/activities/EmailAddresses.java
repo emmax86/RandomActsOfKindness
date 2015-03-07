4fde8b8cdb799447f547dfcf1929e365f08f9d0f
Nathan Fegard <nfegard@asu.edu>
2015-03-07 15:19:01 -0700

    jesus take the wheel

--- android/app/src/main/java/activities/EmailAddresses.java
+++ android/app/src/main/java/activities/EmailAddresses.java
@@ -1,9 +1,13 @@
 package activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 
 import me.dstny.activities.R;
 
@@ -21,4 +25,38 @@ public class EmailAddresses extends Activity {
 
     }
 
+    public void emailAddPressed(View view) {
+        AlertDialog.Builder builder = new   AlertDialog.Builder(EmailAddresses.this);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setTitle("Email");
+        alertDialog.setMessage("Enter an email of a good friend or family member.");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_PHONE);
+        alertDialog.setView(input);
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Confirm",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int whichButton) {
+                        // code that checks to see if the email is valid or used by another account
+                        // if it passes checks make it the new email and close alert dialog
+
+                    }
+
+                });
+
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int whichButton) {
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public void onBackPressed() {
+        Intent intent = new Intent(EmailAddresses.this, Settings.class );
+        startActivity(intent);
+        finish();
+    }
 }
