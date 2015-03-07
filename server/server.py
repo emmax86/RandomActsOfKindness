082d2880a91b8cb8e557a98dfea683cbef1e6f02
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-07 01:18:25 -0700

    Baby's first post request

--- server/server.py
+++ server/server.py
@@ -3,6 +3,37 @@ from subprocess import call
 
 app = Flask(__name__)
 
+"""
+"""
+
+from flask import request
+from flask import abort
+from flask import jsonify
+
+objects = []
+
+@app.route('/create', methods=['POST'])
+def create_object():
+    if not request.json or not 'important' in request.json:
+        abort(400)
+    if len(objects) == 0:
+        someobject = {
+        'id': 0,
+        'important': request.json['important'],
+        'nonreq': request.json.get('nonreq', "no nonreq input"),
+        }
+    else:
+        someobject = {
+        'id': objects[-1]["id"] + 1,
+        'important': request.json['important'],
+        'nonreq': request.json.get('nonreq', "no nonreq input"),
+        }
+
+    objects.append(someobject)
+    return jsonify({'object': someobject}), 201
+
+"""
+"""
 
 @app.route('/')
 def hello_world():
@@ -16,3 +47,7 @@ def update():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+
+    """
+    host='0.0.0.0', port=80, debug=True
+    """
