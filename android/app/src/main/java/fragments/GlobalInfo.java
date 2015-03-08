edeec7fd958576528596be2f550d7cc066a5f5f8
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-08 08:07:15 -0700

    Stable

--- android/app/src/main/java/fragments/GlobalInfo.java
+++ android/app/src/main/java/fragments/GlobalInfo.java
@@ -17,8 +17,6 @@ import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 
-import holo.Bar;
-import holo.BarGraph;
 import me.dstny.activities.R;
 
 public class GlobalInfo extends Fragment {
@@ -29,37 +27,6 @@ public class GlobalInfo extends Fragment {
         View view = inflater.inflate(R.layout.fragment_globalinfo, container, false);
 
         // Get Global Data
-
-
-        int CALLS_VALUE = 5;
-        int POSTS_VALUE = 3;
-        int DONATIONS_VALUE = 7;
-        int MAIL_VALUE = 6;
-        // Add to the chart
-        ArrayList<Bar> points = new ArrayList<Bar>();
-        Bar d = new Bar();
-        d.setColor(Color.parseColor("#F2385A"));
-        d.setName("Calls");
-        d.setValue(CALLS_VALUE);
-        Bar d2 = new Bar();
-        d2.setColor(Color.parseColor("#F5A503"));
-        d2.setName("Posts");
-        d2.setValue(POSTS_VALUE);
-        points.add(d);
-        points.add(d2);
-        d2.setColor(Color.parseColor("#56D9CD"));
-        d2.setName("Donations");
-        d2.setValue(DONATIONS_VALUE);
-        points.add(d);
-        points.add(d2);
-        d2.setColor(Color.parseColor("#3AA1BF"));
-        d2.setName("Mail");
-        d2.setValue(MAIL_VALUE);
-        points.add(d);
-        points.add(d2);
-
-        BarGraph g = (BarGraph)view.findViewById(R.id.graph);
-        g.setBars(points);
 
         return view;
     }
--- android/app/src/main/java/fragments/UserInfo.java
+++ android/app/src/main/java/fragments/UserInfo.java
@@ -11,8 +11,6 @@ import android.widget.Button;
 import java.util.ArrayList;
 
 import activities.MainActivity;
-import holo.Bar;
-import holo.BarGraph;
 import me.dstny.activities.R;
 
 public class UserInfo  extends Fragment {
@@ -23,37 +21,6 @@ public class UserInfo  extends Fragment {
 
 
         // Get Global Data
-
-
-        int CALLS_VALUE = 5;
-        int POSTS_VALUE = 3;
-        int DONATIONS_VALUE = 7;
-        int MAIL_VALUE = 6;
-        // Add to the chart
-        ArrayList<Bar> points = new ArrayList<Bar>();
-        Bar d = new Bar();
-        d.setColor(Color.parseColor("#F2385A"));
-        d.setName("Calls");
-        d.setValue(CALLS_VALUE);
-        Bar d2 = new Bar();
-        d2.setColor(Color.parseColor("#F5A503"));
-        d2.setName("Posts");
-        d2.setValue(POSTS_VALUE);
-        points.add(d);
-        points.add(d2);
-        d2.setColor(Color.parseColor("#56D9CD"));
-        d2.setName("Donations");
-        d2.setValue(DONATIONS_VALUE);
-        points.add(d);
-        points.add(d2);
-        d2.setColor(Color.parseColor("#3AA1BF"));
-        d2.setName("Mail");
-        d2.setValue(MAIL_VALUE);
-        points.add(d);
-        points.add(d2);
-
-        BarGraph g = (BarGraph)view.findViewById(R.id.graph);
-        g.setBars(points);
 
 
         return view;
