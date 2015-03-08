b9f62f4869e6c1e4a390cf0a7df60b759e50cac0
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-08 04:12:58 -0700

    Fix Strings

--- android/app/src/main/java/fragments/GlobalInfo.java
+++ android/app/src/main/java/fragments/GlobalInfo.java
@@ -8,7 +8,11 @@ import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.ChartData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 
@@ -24,6 +28,8 @@ public class GlobalInfo extends Fragment {
         BarChart chart = (BarChart) view.findViewById(R.id.chart);
 
         // Get Global Data
+
+
         // Add to the chart
         ArrayList<String> xVals = new ArrayList<String>();
         xVals.add("x1");
@@ -37,6 +43,11 @@ public class GlobalInfo extends Fragment {
         vals.add(new BarEntry(6,0));
         vals.add(new BarEntry(7,0));
 
+        ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+        dataSets.add(new BarDataSet(vals,xVals.get(0)));
+
+        BarData data = new BarData(xVals, dataSets);
+        chart.setData(data);
         //https://github.com/PhilJay/MPAndroidChart/wiki/Setting-Data
 
         chart.invalidate();
