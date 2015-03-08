76db875c3468fe07165bac79cda6e4c10704f986
Nathan Fegard <nfegard@asu.edu>
2015-03-07 23:26:03 -0700

    jesus take the wheel

--- android/app/src/main/java/activities/MainActivity.java
+++ android/app/src/main/java/activities/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -83,7 +84,7 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
-    public void assembleButtons(Button act, Button call, Button post, Button donate, Button mail) {
+    public void assembleButtons(Button act, Button call, Button post, Button donate, Button mail, TextView cstats, TextView pstats, TextView dstats, TextView mstats) {
 
         actButton = act;
         callButton = call;
@@ -91,35 +92,128 @@ public class MainActivity extends Activity {
         donateButton = donate;
         mailButton = mail;
 
+        callStats = cstats;
+        socialStats = pstats;
+        donateStats = dstats;
+        mailStats = mstats;
+
         PreferencesLayer preferencesLayer = PreferencesLayer.getInstance();
         callButton.setActivated(preferencesLayer.getCallPref());
         postButton.setActivated(preferencesLayer.getPostPref());
         donateButton.setActivated(preferencesLayer.getDonatePref());
         mailButton.setActivated(preferencesLayer.getMailPref());
+
+        if(callButton.isActivated()) {
+            callStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        if(postButton.isActivated()) {
+            socialStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        if(donateButton.isActivated()) {
+            donateStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        if(mailButton.isActivated()) {
+            mailStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
     }
 
     public void kindnessButtonPressed(View view) {
+        if(callButton.isActivated() || postButton.isActivated() || donateButton.isActivated() || mailButton.isActivated()) {
+            ArrayList<Button> activatedButtons = new ArrayList<Button>();
+            if(callButton.isActivated()) {
+                activatedButtons.add(callButton);
+            }
+            if(postButton.isActivated()) {
+                activatedButtons.add(postButton);
+            }
+            if(donateButton.isActivated()) {
+                activatedButtons.add(donateButton);
+            }
+            if(mailButton.isActivated()) {
+                activatedButtons.add(mailButton);
+            }
+            Button selectedButton = activatedButtons.get(randomInt(activatedButtons.size()));
+
+            if(selectedButton.equals(callButton)) {
+                randomCall();
+            }
+            else if (selectedButton.equals(postButton)) {
+                randomPost();
+            }
+            else if (selectedButton.equals(donateButton)) {
+                randomDonate();
+            }
+            else {
+                randomMail();
+            }
+        }
+        else {
+            Toast.makeText(this, "Please select a category in order to take part in Random Acts of Kindness.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public int randomInt(int n) {
+        return new Random().nextInt(n);
+    }
+
+    public void randomCall() {
+
+    }
+
+    public void randomPost() {
+
+    }
+
+    public void randomDonate() {
+
+    }
+
+    public void randomMail() {
 
     }
 
     public void callButtonPressed(View view) {
         callButton.setActivated(!callButton.isActivated());
         PreferencesLayer.getInstance().setCallPref(callButton.isActivated());
+        if(callButton.isActivated()) {
+            callStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        else {
+            callStats.setBackgroundResource(R.mipmap.orange_circle_filled);
+        }
     }
 
     public void postButtonPressed(View view) {
         postButton.setActivated(!postButton.isActivated());
         PreferencesLayer.getInstance().setPostPref(postButton.isActivated());
+        if(postButton.isActivated()) {
+            socialStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        else {
+            socialStats.setBackgroundResource(R.mipmap.orange_circle_filled);
+        }
     }
 
     public void donateButtonPressed(View view) {
         donateButton.setActivated(!donateButton.isActivated());
         PreferencesLayer.getInstance().setDonatePref(donateButton.isActivated());
+        if(donateButton.isActivated()) {
+            donateStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        else {
+            donateStats.setBackgroundResource(R.mipmap.orange_circle_filled);
+        }
     }
 
     public void mailButtonPressed(View view) {
         mailButton.setActivated(!mailButton.isActivated());
         PreferencesLayer.getInstance().setMailPref(mailButton.isActivated());
+        if(mailButton.isActivated()) {
+            mailStats.setBackgroundResource(R.mipmap.l_blue_circle_filled);
+        }
+        else {
+            mailStats.setBackgroundResource(R.mipmap.orange_circle_filled);
+        }
     }
 
     public void settingsButtonPressed(View view) {
--- android/app/src/main/java/fragments/Home.java
+++ android/app/src/main/java/fragments/Home.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import activities.MainActivity;
 import me.dstny.activities.R;
@@ -20,7 +21,12 @@ public class Home extends Fragment{
         Button postButton = (Button) view.findViewById(R.id.post_button);
         Button donateButton = (Button) view.findViewById(R.id.donate_button);
         Button mailButton = (Button) view.findViewById(R.id.mail_button);
-        ((MainActivity) getActivity()).assembleButtons(actButton, callButton, postButton, donateButton, mailButton);
+        TextView callText = (TextView) view.findViewById(R.id.call_count);
+        TextView postText = (TextView) view.findViewById(R.id.post_count);
+        TextView donateText = (TextView) view.findViewById(R.id.donate_count);
+        TextView mailText = (TextView) view.findViewById(R.id.mail_count);
+                ((MainActivity) getActivity()).assembleButtons(actButton, callButton, postButton, donateButton,
+                                                               mailButton, callText, postText, donateText, mailText);
         return view;
     }
 
