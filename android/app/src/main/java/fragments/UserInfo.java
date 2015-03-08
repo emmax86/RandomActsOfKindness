656de095cceeb00ff267332b7846ab7bacb236ba
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-07 20:14:40 -0700

    Prep for Nathan

---
+++ android/app/src/main/java/fragments/UserInfo.java
@@ -0,0 +1,21 @@
+package fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import activities.MainActivity;
+import me.dstny.activities.R;
+
+public class UserInfo  extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+
+        View view = inflater.inflate(R.layout.fragment_userinfo, container, false);
+
+        return view;
+    }
+}
